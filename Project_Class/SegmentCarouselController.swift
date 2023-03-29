5d775d1f87820e80dc173546ade2c375583a5420
Humanken <3hotpeper@gmail.com>
2023-03-29 22:25:00 +0800

    add annotation

--- Project_Class/SegmentCarouselController.swift
+++ Project_Class/SegmentCarouselController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 蔡濬桔 on 2023/3/29.
 //
+//  圖像輪播 控制
 
 import UIKit
 
@@ -16,42 +17,54 @@ class SegmentCarouselController: UIViewController, UICollectionViewDataSource, U
     var timerAnimate: Timer?
     
     @IBOutlet weak var LabelStatus: UILabel!
-    @IBOutlet weak var Swich: UISwitch!
+    @IBOutlet weak var Swich: UISwitch! // 開關
+    // 使用 collection view 放入多個圖像
     @IBOutlet weak var myCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 設定 collection view 位置與大小
         self.setCollectionViewSize(collectionView: myCollectionView)
+        // collection view 初始設定
         myCollectionView.dataSource = self
         myCollectionView.delegate = self
+        // 輪播 計時器
         timerAnimate = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(changeImg), userInfo: nil, repeats: true)
     }
     
+    /* 當 滑桿值 變更 -> 設定collection view 位置與大小 */
     @IBAction func valueChange_slider(_ sender: UISlider) {
         sliderValue = Int(sender.value * 10)
         self.setCollectionViewSize(collectionView: myCollectionView)
     }
     
+    /* 設定collection view 位置與大小 */
     func setCollectionViewSize(collectionView: UICollectionView){
         let imgSize = sliderValue
+        // 取得 計算後的左上角座標
         let loc = self.calculateLoc()
         
         collectionView.frame = CGRect(x: CGFloat(loc[0]), y: CGFloat(loc[1]), width: CGFloat(imgSize), height: CGFloat(imgSize))
     }
     
+    /* 計算 置中位置 -> 左上角座標 */
     func calculateLoc() -> [Int]{
         let imgSize = sliderValue
+        // 元件 初始位置 與 初始大小
         let LocX = 40
         let LocY = 172
         let oriImgSize = 292
+        
         var calculate = 0
-        if imgSize < 292{
+        // 若元件大小 == 原始大小，則calculate = 0
+        // 避免用0除報錯
+        if imgSize < oriImgSize{
             calculate = (oriImgSize - imgSize)/2
         }
         return [calculate + LocX, calculate + LocY]
     }
     
-    
+    /* 當 開關值 變更 -> 啟用/禁用 輪播計時器 */
     @IBAction func valueChange_swich(_ sender: UISwitch) {
         if sender.isOn{
             LabelStatus.text = "圖片輪播：開啟"
@@ -64,6 +77,7 @@ class SegmentCarouselController: UIViewController, UICollectionViewDataSource, U
         }
     }
     
+    /* 切換圖像 @objc: 給計時器提供#selector用法 */
     @objc func changeImg(){
         imageIndex += 1
         if imageIndex < images.count{
@@ -78,37 +92,44 @@ class SegmentCarouselController: UIViewController, UICollectionViewDataSource, U
         }
     }
     
+    /* 限制 section 數量 -> return 1 */
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
+    /* 限制 項目 數量 -> return 圖像總數量 */
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
     
+    /* 控制cell -> return cell */
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
         cell.ImgView.image = UIImage(named: images[indexPath.item])
         return cell
     }
     
-    
+    /* 控制cell大小 -> return collection view大小 */
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return collectionView.bounds.size
     }
     
+    /* 設定 行間隔 -> return 0 */
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
     
+    /* 設定 每個item之間的間隔 -> return 0 */
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
 }
 
 
+/* 自定 collection view cell 類別 */
 class MyCollectionViewCell: UICollectionViewCell{
     
+    // 命名 UIImageView 元件
     @IBOutlet weak var ImgView: UIImageView!
     
 }
--- Project_Class/SegmentImageConotroller.swift
+++ Project_Class/SegmentImageConotroller.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 蔡濬桔 on 2023/3/29.
 //
+//  圖像自動切換 控制
 
 import UIKit
 
@@ -16,15 +17,20 @@ class SegmentImageController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 設定 image view 位置與大小
         controlImgView(sliderValue: 14.6)
+        // 設定 圖像切換動畫
         setAnimateImgView(count: 4, duration: 5.0)
+        // 動畫 啟用
         ImageView.startAnimating()
     }
     
+    /* 當 滑桿值 變更 -> 設定 image view 位置與大小 */
     @IBAction func valueChange_slider(_ sender: UISlider) {
         controlImgView(sliderValue: sender.value)
     }
     
+    /* 當 開關值 變更 -> 啟用/禁用 輪播計時器 */
     @IBAction func valueChange_swich(_ sender: UISwitch) {
         if sender.isOn{
             ImageView.startAnimating()
@@ -36,6 +42,7 @@ class SegmentImageController: UIViewController {
         }
     }
     
+    /* 設定 圖像切換動畫 */
     func setAnimateImgView(count: Int, duration: Double){
         var imgs = [UIImage]()
         for i in 1...count{
@@ -46,19 +53,25 @@ class SegmentImageController: UIViewController {
         ImageView.animationDuration = duration * Double(count)
     }
     
+    /* 設定 image view 位置與大小 */
     func controlImgView(sliderValue: Float){
         let imgSize = sliderValue * 10
         let loc = calculateImgViewLoc(sliderValue: sliderValue)
         ImageView.frame = CGRect(x: CGFloat(loc[0]), y: CGFloat(loc[1]), width: CGFloat(imgSize), height: CGFloat(imgSize))
     }
     
+    /* 計算 置中位置 -> 左上角座標 */
     func calculateImgViewLoc(sliderValue: Float) -> [Int]{
         let imgSize = Int(sliderValue * 10)
+        // 元件 初始位置 與 初始大小
         let LocX = 40
         let LocY = 172
         let oriImgSize = 292
+        
         var calculate = 0
-        if imgSize < 292{
+        // 若元件大小 == 原始大小，則calculate = 0
+        // 避免用0除報錯
+        if imgSize < oriImgSize{
             calculate = (oriImgSize - imgSize)/2
         }
         return [calculate + LocX, calculate + LocY]
